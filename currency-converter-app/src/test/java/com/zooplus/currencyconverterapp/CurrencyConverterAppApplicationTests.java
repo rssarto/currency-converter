07344c8646487112422b93502423854e0bf22931
Ricardo Sarto <rssarto@rssarto-Inspiron-3647>
2018-03-08 16:19:01 -0300

    tests

--- currency-converter-app/src/test/java/com/zooplus/currencyconverterapp/CurrencyConverterAppApplicationTests.java
+++ currency-converter-app/src/test/java/com/zooplus/currencyconverterapp/CurrencyConverterAppApplicationTests.java
@@ -2,12 +2,13 @@ package com.zooplus.currencyconverterapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.converter.app.CurrencyConverterAppApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes={SpringBootApplication.class})
+@SpringBootTest(classes={CurrencyConverterAppApplication.class})
 public class CurrencyConverterAppApplicationTests {
 
 	@Test
