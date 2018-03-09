eb5b1eec86f502ca6716d530c863f2c3c53ff352
rssarto <ricardo.soares.sarto@gmail.com>
2018-03-08 23:06:23 -0300

    database connection

--- currency-converter-app/src/main/java/com/converter/config/WebConfig.java
+++ currency-converter-app/src/main/java/com/converter/config/WebConfig.java
@@ -1,7 +1,10 @@
 package com.converter.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -27,9 +30,38 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		return new RestTemplate();
 	}
 	
+	@Bean
 	public ObjectMapper objectMapper() {
 		return new ObjectMapper();
 	}
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public BasicDataSource dataSource() {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        if( dbUrl == null ) {
+        	dbUrl = env.getProperty("spring.datasource.url");
+        }
+        
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        if( username == null ) {
+        	username = env.getProperty("spring.datasource.username");
+        }
+        
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+        if( password == null ) {
+        	password = env.getProperty("spring.datasource.password");
+        }
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
 }
 
