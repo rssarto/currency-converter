8bce78460a121cf11cce8bcb56e31fa97288feff
rssarto <ricardo.soares.sarto@gmail.com>
2018-03-11 17:23:02 -0300

    tests

--- currency-converter-app/src/main/java/com/converter/dao/UserDao.java
+++ currency-converter-app/src/main/java/com/converter/dao/UserDao.java
@@ -1,12 +1,12 @@
 package com.converter.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.converter.model.User;
 
 @Repository
-public interface UserDao extends CrudRepository<User, Long> {
+public interface UserDao extends PagingAndSortingRepository<User, Long> {
 	
 	User findByUserName(String userName);
 	User findByEmail(String email);
--- currency-converter-app/src/test/java/com/currencyconverter/UserControllerTest.java
+++ currency-converter-app/src/test/java/com/currencyconverter/UserControllerTest.java
@@ -1,24 +1,33 @@
 package com.currencyconverter;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.converter.app.CurrencyConverterAppApplication;
 import com.converter.controller.UserController;
+import com.converter.dao.UserDao;
+import com.converter.exception.SignUpException;
 import com.converter.model.User;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -35,22 +44,38 @@ public class UserControllerTest {
 	@Autowired
 	private MockMvc mvc;
 	
+	@Autowired
+	private UserDao userDao;
+	
 	@Test
 	public void addNewUser() throws Exception {
-		int randomInt = ThreadLocalRandom.current().nextInt();
-		User user = new User("teste" + randomInt, "123456", "teste" + randomInt + "@teste.com.br", "Rua Marcondes do Amaral, " + randomInt, "03661-000", "SAO PAULO", "BRAZIL");
-		this.addUser(user, HttpStatus.OK);
+		User user = getNewUser();
+		this.addUser(user).andExpect(status().isOk());
 	}
 	
-	private void addUser(User user, HttpStatus expectedStatus) throws Exception {
-		this.mvc.perform(post("/signup")
-				.contentType(MediaType.APPLICATION_JSON_VALUE).content(toJson(user)))
-			 .andExpect(expectedStatus == HttpStatus.OK ? status().isOk() : status().isBadRequest());		
+	@Test(expected=Exception.class)
+	public void addDuplicateUserName()throws Exception{
+		Page<User> pageRegisteredUser = userDao.findAll(new PageRequest(0, 1));
+		User registeredUser = pageRegisteredUser.getContent().get(0);
+		Assert.assertNotNull(registeredUser);
+		User newUser = getNewUser();
+		newUser.setUserName(registeredUser.getUserName());
+		this.addUser(newUser);
+	}
+	
+	private ResultActions addUser(User user) throws Exception {
+		return this.mvc.perform(post("/signup")
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(toJson(user)));
 	}
 	
 	private void getUser(int customerId, HttpStatus expectedStatus)throws Exception{
 		this.mvc.perform(get("/user/" + customerId)).andExpect(expectedStatus == HttpStatus.OK ? status().isOk() : status().isNotFound());
-	}	
+	}
+	
+	private static User getNewUser() {
+		int randomInt = ThreadLocalRandom.current().nextInt();
+		return new User("teste" + randomInt, "123456", "teste" + randomInt + "@teste.com.br", "Rua Marcondes do Amaral, " + randomInt, "03661-000", "SAO PAULO", "BRAZIL");
+	}
 	
 	private static String toJson(Object object){
 		String json = gson.toJson(object);
