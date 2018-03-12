fe5de5458cb6dc9522f429311ca42c081f43502b
rssarto <ricardo.soares.sarto@gmail.com>
2018-03-12 13:37:37 -0300

    authentication test

--- currency-converter-app/src/test/java/com/currencyconverter/AuthenticationControllerTest.java
+++ currency-converter-app/src/test/java/com/currencyconverter/AuthenticationControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.converter.app.CurrencyConverterAppApplication;
 import com.converter.controller.AuthenticationController;
@@ -15,21 +16,26 @@ import com.converter.controller.UserController;
 import com.converter.model.LoginUser;
 import com.converter.model.User;
 
+/*
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes= {CurrencyConverterAppApplication.class})
 @PropertySource("classpath:application.properties")
 @WebMvcTest(controllers={AuthenticationController.class, UserController.class})
+*/
 public class AuthenticationControllerTest extends AbstractControllerTest {
 	
-	@Test
+	//@Test
 	public void login() throws Exception {
 		UserControllerTest userControllerTest =  new UserControllerTest();
 		userControllerTest.populateDependencies(this);
 		User user = userControllerTest.getNewUser();
 		userControllerTest.addUser(user);
 		LoginUser loginUser = new LoginUser(user.getUserName(), user.getPassword());
-		this.executePostRequest("/token/generate-token", loginUser)
-			.andExpect(status().isOk());
+		authenticate(loginUser).andExpect(status().isOk());
+	}
+
+	public ResultActions authenticate(LoginUser loginUser) throws Exception {
+		return this.executePostRequest("/token/generate-token", loginUser);
 	}
 
 }
